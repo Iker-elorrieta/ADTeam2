9a7850bdeeabe75ffb38db9a26411bd2719e400b
asier <asier@192.168.1.137>
2020-10-23 06:06:03 +0200

    Adaptado RellenaLibro con los metodos de Validacion de Jonathan

--- src/metodosComunes/RellenarLibro.java
+++ src/metodosComunes/RellenarLibro.java
@@ -10,39 +10,69 @@ public class RellenarLibro {
 
 	public static Libro rellenarLibro() {
 
-		String titulo ;
-		String editorial ;
-		String paginas ;
-		String altura ;
-		String notas ;
-		String isbn ;
-		String materias; 
-		
+		Validacion v = new Validacion();
+
+		String titulo;
+		String editorial;
+		String paginas;
+		String altura;
+		String notas;
+		String isbn;
+		String materias;
+
 		System.out.println("Introduce un titulo de libro ");
 		titulo = sc.nextLine();
+		while (v.validarTitulo(titulo) == false) {
+			System.out.println("Introduce un titulo de libro ");
+			titulo = sc.nextLine();
+		}
 
 		System.out.println("Introduce una editorial ");
 		editorial = sc.nextLine();
+		while (v.validarEditorial(editorial) == false) {
+			System.out.println("Introduce una editorial ");
+			editorial = sc.nextLine();
+		}
 
 		System.out.println("Introduce un numero de paginas ");
 		paginas = sc.nextLine();
+		while (v.validarPaginas(paginas) == false) {
+			System.out.println("Introduce un numero de paginas ");
+			paginas = sc.nextLine();
+		}
 
 		System.out.println("Introduce un altura ");
 		altura = sc.nextLine();
+		while (v.validarAltura(altura) == false) {
+			System.out.println("Introduce un altura ");
+			altura = sc.nextLine();
+		}
 
 		System.out.println("Introduce un notas ");
 		notas = sc.nextLine();
+		while (v.validarNotas(notas) == false) {
+			System.out.println("Introduce un notas ");
+			notas = sc.nextLine();
+		}
 
 		System.out.println("Introduce una isbn ");
 		isbn = sc.nextLine();
+		while (v.validarIsbn(isbn) == false) {
+			System.out.println("Introduce una isbn ");
+			isbn = sc.nextLine();
+		}
 
 		System.out.println("Introduce una materias ");
 		materias = sc.nextLine();
-
+		while (v.validarMaterias(materias) == false) {
+			System.out.println("Introduce una materias ");
+			materias = sc.nextLine();
+		}
+		
 		Libro libro = new Libro(titulo, editorial, paginas, altura, notas, isbn, materias);
+		
 		return libro;
 
 	}
-	
-	
+
 }
