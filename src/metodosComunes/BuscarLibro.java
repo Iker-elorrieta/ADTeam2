57ff80728f92128187c98075557a0ba0df1802c7
in2dam-b <in2dam-b@HZ061513.elorrieta.int>
2020-11-11 20:02:30 +0100

    Corregido Final en BuscarLibro

--- src/metodosComunes/BuscarLibro.java
+++ src/metodosComunes/BuscarLibro.java
@@ -7,28 +7,32 @@ import objetos.Libro;
 public class BuscarLibro {
 
 	/**
-	 * Esta clase contiene el metodo de Buscar Libro 
-	 * @param libros ArrayList de Libros que se le pasa al metodo como parametro
-	 * @param categoria String que se le pasa al metodo como parametro(Indica por que atributo del Libro se quiere buscar)
-	 * @param dato String que se le pasa al metodo como parametro(patron a buscar)
-	 * @param busqueda Numero entero que se le pasa al metodo como parametro(Indica la zona de busqueda)
+	 * Esta clase contiene el metodo de Buscar Libro
+	 * 
+	 * @param libros    ArrayList de Libros que se le pasa al metodo como parametro
+	 * @param categoria String que se le pasa al metodo como parametro(Indica por
+	 *                  que atributo del Libro se quiere buscar)
+	 * @param dato      String que se le pasa al metodo como parametro(patron a
+	 *                  buscar)
+	 * @param busqueda  Numero entero que se le pasa al metodo como parametro(Indica
+	 *                  la zona de busqueda)
 	 * @author Asier, Jonatan
 	 * @return librosencontrados ArrayList de Libros encontrados en la busqueda
-	 * @throws IOException clase general de excepciones producidas por operaciones de E / S fallidas o interrumpidas.
+	 * @throws IOException clase general de excepciones producidas por operaciones
+	 *                     de E / S fallidas o interrumpidas.
 	 */
-	
+
 	public static ArrayList<Libro> buscar(ArrayList<Libro> libros, String categoria, String dato, int busqueda) {
 
 		ArrayList<Libro> librosEncontrados = new ArrayList<>();
-		
+
 		final String titulo = "titulo";
-		final String editorial = "editorial"; 
-		final String paginas = "paginas"; 
-		final String altura = "altura"; 
-		final String notas = "notas"; 
-		final String isbn = "isbn"; 
-		final String materias = "materias"; 
-		
+		final String editorial = "editorial";
+		final String paginas = "paginas";
+		final String altura = "altura";
+		final String notas = "notas";
+		final String isbn = "isbn";
+		final String materias = "materias";
 
 		for (Libro libro : libros) {
 
